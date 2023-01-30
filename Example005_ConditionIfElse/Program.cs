a672125eca973e78d047563b2dde9db306ca0d7a
Denis Ananchenko <denis@ananchenko.ru>
2023-01-30 21:30:39 +0700

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Fifth project on lection #№1
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же МАША!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
