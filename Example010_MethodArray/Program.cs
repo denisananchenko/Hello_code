0f96a4a2ba6fecfe4be54e8df90e11631c41c761
Denis Ananchenko <denis@ananchenko.ru>
2023-02-07 22:36:57 +0700

    add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// Tenth project on lection #№2
+int[] array = { 11, 2, 13, 41, 15, 61, 71, 15, 8 };
+
+int n = array.Length;
+int find = 15;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
